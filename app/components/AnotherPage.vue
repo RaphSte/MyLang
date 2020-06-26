492e91b0b7a8dc8f224242a9e11eabb219e18bd6
raphste <raphael.sterk@johner-institut.de>
2020-06-26 18:58:38 +0200

    # initial commit

---
+++ app/components/AnotherPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <Page>
+        <ActionBar title="AnotherPage!!!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="" text="AnotherPage!!! Awesome!!!" col="0" row="0"/>
+			<Button class="button" :text="btnmsg" @tap="goback" col="0" row="0"/>
+			
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+
+	import App from './App';
+
+  export default {
+    data() {
+      return {
+        msg: 'tap da button!!!!1!1',
+		counter: 1,
+		btnmsg: 'tap me'
+      }
+    },
+	
+	methods: {
+	goback(){
+		this.$navigateTo(App)
+	}
+  }
+	
+
+    };
+</script>
+
+<style scoped>
+	ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+	.button {
+		background-color: purple;
+	}
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: white;
+    }
+</style>
---
+++ app/components/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+		<Label class="" :text="msg" col="0" row="0"/>
+			<Button class="button" :text="btnmsg" @tap="onTap" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+  import AnotherPage from './AnotherPage';
+  
+  
+  export default {
+    
+		
+	data() {
+      return {
+        msg: 'tap da button!!!!1!1',
+		counter: 1,
+		btnmsg: 'tap me'
+      }
+    },
+
+  methods: {
+	onTap(){
+		this.btnmsg = "YES! TAP ME HARDER DADDY!! UwU" + this.counter
+		this.counter = this.counter +1
+		this.$navigateTo(AnotherPage)
+	}
+  }
+    };
+</script>
+
+<style scoped>
+	
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+	.button {
+		background-color: pink;
+	}
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: white;
+    }
+</style>
