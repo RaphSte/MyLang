e88799a3e362b278cb2d5697fbe9d88b9e4cb348
raphste <raphael.sterk@johner-institut.de>
2020-07-04 00:39:12 +0200

    #list view

--- app/components/Topics.vue
+++ app/components/Topics.vue
@@ -1,12 +1,59 @@
 <template>
-    <Label text="Topics!" />
+    <!--    <StackLayout class="custom-list-wrapper">-->
+
+
+    <StackLayout class="custom-list-wrapper" orientation="vertical">
+        <Label text="vocabularies" class="lab" verticalAlignment="center"/>
+        <ListView class="list-group" for="item in allVocabularies" backgroundColor="transparent" separatorColor="transparent">
+            <StackLayout class="custom-list-wrapper" orientation="vertical">
+                <v-template>
+                    <Label :text="item.name" class="lab" verticalAlignment="center"/>
+                </v-template>
+            </StackLayout>
+        </ListView>
+    </StackLayout>
+
+
 </template>
-<script>
+<script lang="ts">
+
+    import {Vocabularies} from "../data/vocabularies";
+
     export default {
-        name: "Topics"
+        name: "Topics",
+        data() {
+            return {
+                vocabularies: Vocabularies,
+                allVocabularies: [],
+                myTitles: [
+                    {id: 1, title: "The Da Vinci Code"},
+                    {id: 2, name: "Harry Potter and the Chamber of Secrets"}
+                ]
+            };
+        },
+
+        methods: {
+            getVocabularies() {
+                this.vocabularies.forEach((vocabulary, index) => {
+                    this.allVocabularies.push({
+                        name: vocabulary.english
+                    });
+                    console.log("push");
+                });
+            }
+        },
+        created() {
+            this.getVocabularies();
+        }
     }
 </script>
 
+
 <style scoped>
+    .custom-list-wrapper {
+        margin: 10;
+        padding: 5;
+    }
+
 
 </style>
---
+++ app/data/vocabularies.ts
@@ -0,0 +1,101 @@
+export const Vocabularies = [
+    {
+        id: 1,
+        category: "food",
+        english: "Sugar",
+        german: "Zucker",
+        thai: "น้ำตาล",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 2,
+        category: "food",
+        english: "Meat",
+        german: "Fleisch",
+        thai: "เนื้อ",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 3,
+        category: "food",
+        english: "Knife",
+        german: "Messer",
+        thai: "มีด",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 4,
+        category: "food",
+        english: "Cheese",
+        german: "Käse",
+        thai: "ชีส",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 5,
+        category: "food",
+        english: "Egg",
+        german: "Ei",
+        thai: "มีด",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 6,
+        category: "Nature",
+        english: "Sun",
+        german: "Sonne",
+        thai: "ดวงอาทิตย์",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 7,
+        category: "Nature",
+        english: "Wind",
+        german: "Wind",
+        thai: "ลม",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    },
+    {
+        id: 8,
+        category: "Nature",
+        english: "Rain",
+        german: "Regen",
+        thai: "ฝน",
+        repetitions: 0,
+        correct_learned: 0,
+        incorrect_learned: 0,
+        icon_id: "",
+        sound_sample: ""
+    }
+]
+
+
+
